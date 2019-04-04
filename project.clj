ee26f26a4888957ad2e3eae7c36646a6a6a6b7cc
Yu Zhao <yu.zhao@joudou.com>
2019-04-04 17:11:35 +0800

    update

--- project.clj
+++ project.clj
@@ -3,9 +3,30 @@
   :url "http://www.parens-of-the-dead.com"
   :license {:name "GNU General Public License"
             :url  "http://www.gnu.org/licenses/gpl.html"}
-  :jvm-opts ["-XX:MaxPermSize=256m"]
-  :dependencies [[org.clojure/clojure "1.7.0"]]
-  :profiles {:dev {:plugins      []
-                   :dependencies []
-                   :source-paths ["dev"]}}
+  :main undead.system
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.562"]
+                 [com.stuartsierra/component "0.3.2"]
+                 [http-kit "2.2.0"]
+                 [compojure "1.6.0"]
+                 ]
+  :plugins [[lein-figwheel "0.5.10"]]
+  :figwheel {:server-port 3450
+             }
+  :cljsbuild {:builds [{:id           "dev"
+                        :source-paths ["src" "dev"]
+                        :figwheel     true
+                        :compiler     {:main                 "undead.client"
+                                       :asset-path           "/out"
+                                       :output-to            "target/classes/public/app.js"
+                                       :output-dir           "target/classes/public/out"
+                                       :optimizations        :none
+                                       :recompile-dependents true
+                                       :source-map           true}}]}
+  :profiles {:dev {:plugins      [[lein-cljsbuild "1.1.6"]
+                                  ]
+                   :dependencies [[reloaded.repl "0.2.3"]]
+                   :source-paths ["dev"]
+                   }
+             }
   )
